24db27ae180c9c2c0c7bde3450662473895a400b
Sushovan <sushovanghosh960@gmail.com>
2024-03-24 20:14:48 +0530

    add script files
    use canvas html elemt to build the road
    use class to create an Car instance
    use canvas methods and functionality to draw a card on the canvas

---
+++ modules/car.js
@@ -0,0 +1,23 @@
+import Controls from "./Controls.js";
+
+export default class Car {
+    constructor(x, y, height, width) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+
+        this.controls = new Controls();
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.rect(
+            this.x - this.width / 2,
+            this.y - this.height / 2,
+            this.width,
+            this.height,
+        );
+        ctx.fill();
+    }
+}
