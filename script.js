e111d13fc641f34b5fe242d99bb3debadf2b0dad
Sushovan <sushovanghosh960@gmail.com>
2024-03-25 22:04:25 +0530

    add lane dash
    add lane dash to the lane divisions; the middle divisions will be dashed only. (i> 0 <lanecount)
    create a method that will center the car object based on the lane division

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ import Road from "./modules/Roads.js";
 import Car from "./modules/car.js"
 
 const canvas = document.getElementById('myCanvas');
-canvas.width = 200;
+canvas.width = 240;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(100, 100, 50, 30);
+const car = new Car(road.getLaneCenter(1), 100, 50, 30);
 
 (function animate() {
     car.update();
