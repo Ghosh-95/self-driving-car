7dbe726aa91e33b54f72033261eb4680ca89df2f
Sushovan <sushovanghosh960@gmail.com>
2024-03-25 22:21:48 +0530

    add moving road
    road will move respective to the car object

--- script.js
+++ script.js
@@ -13,7 +13,14 @@ const car = new Car(road.getLaneCenter(1), 100, 50, 30);
 (function animate() {
     car.update();
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.7);
+
     road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
+
     requestAnimationFrame(animate);
 })();
