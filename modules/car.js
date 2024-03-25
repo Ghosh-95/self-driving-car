2e2024d59f5a7b11eef81687c56821b7f29e09eb
Sushovan <sushovanghosh960@gmail.com>
2024-03-25 14:22:10 +0530

    fix: rotate while car is in speed
    object rotates only when it has a speed
    set the direction towards the rotation of the object.

--- modules/car.js
+++ modules/car.js
@@ -18,10 +18,10 @@ export default class Car {
 
     draw(ctx) {
         ctx.save();
-        ctx.beginPath();
         ctx.translate(this.x, this.y);
         ctx.rotate(-this.angle);
 
+        ctx.beginPath();
         ctx.rect(
             - this.width / 2,
             - this.height / 2,
@@ -48,10 +48,15 @@ export default class Car {
         if (this.speed < 0) this.speed += this.friction;
         if (Math.abs(this.speed) < this.friction) this.speed = 0;
 
-        if (this.controls.right) this.angle -= 0.03;
-        if (this.controls.left) this.angle += 0.03;
+        if (this.speed !== 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.right) this.angle -= 0.03 * flip;
+            if (this.controls.left) this.angle += 0.03 * flip;
+        }
 
-        this.y -= this.speed;
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+
 
     }
 }
