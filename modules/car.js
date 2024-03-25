3c0b928fae49808a13b8a1174b844e29a8b54f88
Sushovan <sushovanghosh960@gmail.com>
2024-03-25 14:02:47 +0530

    add right and left functionality
    add a new variable angle which will increase and decrease by 0.03 after going left and right respictively
    translate the ctx with x and y, remove x and y from rect().
    use restore() method to avoid infinite drawing.

--- modules/car.js
+++ modules/car.js
@@ -11,19 +11,25 @@ export default class Car {
         this.acceleration = 0.2;
         this.maxSpeed = 3;
         this.friction = 0.05;
+        this.angle = 0;
 
         this.controls = new Controls();
     }
 
     draw(ctx) {
+        ctx.save();
         ctx.beginPath();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            - this.height / 2,
             this.width,
             this.height,
         );
         ctx.fill();
+        ctx.restore();
     }
 
     update() {
@@ -42,6 +48,10 @@ export default class Car {
         if (this.speed < 0) this.speed += this.friction;
         if (Math.abs(this.speed) < this.friction) this.speed = 0;
 
+        if (this.controls.right) this.angle -= 0.03;
+        if (this.controls.left) this.angle += 0.03;
+
         this.y -= this.speed;
+
     }
 }
