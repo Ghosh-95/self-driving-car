457a2f110cf46502311994a561702831a54676fb
Sushovan <sushovanghosh960@gmail.com>
2024-03-25 21:23:15 +0530

    add roads

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 "use strict";
 
+import Road from "./modules/Roads.js";
 import Car from "./modules/car.js"
 
 const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(100, 100, 50, 30);
 
 (function animate() {
     car.update();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 })();
